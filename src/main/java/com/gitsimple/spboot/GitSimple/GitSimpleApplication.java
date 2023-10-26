85ea4bf3dd8b16bd1bd985142e92849748bdd304
unknown <zinnia@959704@gmail.com>
2023-10-26 16:02:26 +0630

    Signed-off-by: unknown <zinnia@959704@gmail.com>

---
+++ src/main/java/com/gitsimple/spboot/GitSimple/GitSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.gitsimple.spboot.GitSimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitsimple/spboot/GitSimple/GitSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitsimple.spboot.GitSimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
